3e2b0436f371fbe1665de350d0992efd1b7d8752
hubciu30 <szkolny98@wp.pl>
2021-01-24 17:05:34 +0100

    testy

--- zaliczenie/rental/movies/tests.py
+++ zaliczenie/rental/movies/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework.reverse import reverse
+from . import views
+from .models import Client
+from rest_framework import status
 
-# Create your tests here.
+class ClientTest(APITestCase):
+    def post_client(self, sn):
+        url = reverse(views.ClientList.name)
+        dane = {'surname': sn}
+        response = self.client.post(url, dane, format='json')
+        return response
+
+    def test_post_adn_get_osoba(self):
+        new_surname = 'Kowalski'
+        response = self.post_client(new_surname)
+        assert response.status_code == status.HTTP_201_CREATED
+        assert Client.objects.count() == 1
+        assert Client.objects.get().surname == new_surname
